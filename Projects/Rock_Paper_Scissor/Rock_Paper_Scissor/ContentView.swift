c6d67a465c692d0cb1c39dd1849c7f462c9378a6
Suyash-Belwalkar <suyashbel0703@gmail.com>
2024-03-30 23:37:20 +0530

    RPS ui

--- Projects/Rock_Paper_Scissor/Rock_Paper_Scissor/ContentView.swift
+++ Projects/Rock_Paper_Scissor/Rock_Paper_Scissor/ContentView.swift
@@ -8,15 +8,86 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var options = ["✊","✋","✌️"]
+    @State private var shouldWin = ["Win" , "Lose"]
+    @State private var aim = Int.random(in: 0...1)
+    @State private var chose = Int.random(in: 0...2)
+    @State private var score = 0
+    @State private var selected = ""
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            LinearGradient(colors: [.cyan , .black], startPoint: .top, endPoint: .bottom)
+                .ignoresSafeArea()
+            
+            VStack{
+                Text("SCORE: \(score)")
+                    .font(.largeTitle)
+                    .foregroundStyle(.black)
+                    .bold()
+                
+                Text("System Chooses:")
+                    .font(.largeTitle)
+                    .bold()
+                    .foregroundStyle(.gray)
+                
+                Text("\(options[chose])")
+                    .font(.system(size: 80))
+                
+                Text("AIM:  \(shouldWin[aim])")
+                    .font(.title2)
+                    .bold()
+                
+                HStack{
+                    
+                    Button{
+                        
+                    }label: {
+                        Text("✊")
+                            .padding()
+                            .frame(maxWidth:100)
+                            .background(.black)
+                            .clipShape(Circle())
+                            .font(.system(size: 40))
+                    }
+                    
+                    Button{
+                        
+                    }label: {
+                        Text("✌️")
+                            .padding()
+                            .frame(maxWidth:100)
+                            .background(.black)
+                            .clipShape(Circle())
+                            .font(.system(size: 40))
+                    }
+                    
+                    Button{
+                        
+                    }label: {
+                        Text("✋")
+                            .padding()
+                            .frame(maxWidth:100)
+                            .background(.black)
+                            .clipShape(Circle())
+                            .font(.system(size: 40))
+                    }
+                }
+                .frame(height: 150)
+                .background(.clear)
+                .padding(.vertical,40)
+                .clipShape(.rect(cornerRadius: 20))
+            }
+            .frame(maxWidth:370)
+            .padding(.vertical,50)
+            .background(.thinMaterial)
+            .clipShape(.rect(cornerRadius: 20))
         }
-        .padding()
+      
     }
+    
 }
 
 #Preview {
