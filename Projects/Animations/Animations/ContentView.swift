dcabb6131b0ff48d7f40d14e6e426433b3e809cd
Suyash-Belwalkar <suyashbel0703@gmail.com>
2024-07-13 16:15:29 +0530

    intro to animation

--- Projects/Animations/Animations/ContentView.swift
+++ Projects/Animations/Animations/ContentView.swift
@@ -8,14 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animationEffect = 0.0
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack{
+            
+            Button("Suuiii"){
+                withAnimation(.spring(duration: 1,bounce: 0.5)){
+                    animationEffect += 360
+                }
+            }
+            .padding(80)
+            .background(.black)
+            .clipShape(Circle())
+            .rotation3DEffect(.degrees(animationEffect),axis: (x:1, y:1, z:0)
+            )
+         
         }
-        .padding()
+        
     }
 }
 
