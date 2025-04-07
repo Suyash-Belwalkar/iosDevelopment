5492caba0175807039c3bfbd78ef62939c54b5d9
Suyash-Belwalkar <suyashbel0703@gmail.com>
2025-04-08 03:00:29 +0530

    day 40

---
+++ Projects/MoonShot/MoonShot/Astronauts.swift
@@ -0,0 +1,12 @@
+//
+//  Astronauts.swift
+//  MoonShot
+//
+//  Created by Suyash on 07/04/25.
+//
+
+struct Astronauts: Codable, Identifiable{
+    let id: String
+    let name: String
+    let description: String
+}
--- Projects/MoonShot/MoonShot/ContentView.swift
+++ Projects/MoonShot/MoonShot/ContentView.swift
@@ -8,14 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let astronauts: [String: Astronauts] = Bundle.main.decode("astronauts.json")
+    let mission:[Mission] = Bundle.main.decode("missions.json")
+    let columns = [
+        GridItem(.adaptive(minimum: 150))
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            ScrollView{
+                LazyVGrid(columns: columns){
+                    ForEach(mission){mission in
+                        NavigationLink{
+                            Text("Detailed view")
+                        }label:{
+                            VStack{
+                                Image(mission.image)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width:100 , height: 100)
+                                    .padding()
+                                VStack{
+                                    Text(mission.displayName)
+                                        .font(.headline)
+                                        .foregroundStyle(.white)
+                                    
+                                    Text(mission.formattedDate)
+                                        .font(.caption)
+                                        .foregroundStyle(.gray)
+                                    
+                                }
+                                .padding(.vertical)
+                                .frame(maxWidth: .infinity)
+                                .background(.lightBackground)
+                            }
+                            .clipShape(.rect(cornerRadius:10))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(.lightBackground)
+                            )
+                        }
+                    }
+                }
+                .padding([.horizontal , .bottom])
+            }
+            .navigationTitle("MoonShot")
+            .background(.darkBackground)
+            .preferredColorScheme(.dark)
         }
-        .padding()
+        
     }
 }
 
